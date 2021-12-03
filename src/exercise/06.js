c6d250aea2c00f11e658f3ab793dca2b35bfe63c
bdahle <bdahle@hey.com>
2021-12-03 10:30:16 +0100

    06e2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -12,25 +12,33 @@ import {
 function PokemonInfo({pokemonName}) {
   const [pokemon, setPokemon] = React.useState(null)
   const [error, setError] = React.useState('')
+  const [status, setStatus] = React.useState('idle')
 
   React.useEffect(() => {
     if (!pokemonName) return
-    setPokemon(null)
+    setStatus('pending')
     fetchPokemon(pokemonName).then(
-      pokemonData => setPokemon(pokemonData),
-      error => setError(error),
+      pokemonData => {
+        setPokemon(pokemonData)
+        setStatus('resolved')
+      },
+      error => {
+        setError(error)
+        setStatus('rejected')
+      },
     )
   }, [pokemonName])
 
-  if (error)
+  if (status === 'rejected')
     return (
       <div role="alert">
         There was an error:{' '}
         <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
       </div>
     )
-  else if (!pokemonName) return 'Submit a pokemon'
-  else if (!pokemon) return <PokemonInfoFallback name={pokemonName} />
+  else if (status === 'idle') return 'Submit a pokemon'
+  else if (status === 'pending')
+    return <PokemonInfoFallback name={pokemonName} />
   else return <PokemonDataView pokemon={pokemon} />
 }
 
